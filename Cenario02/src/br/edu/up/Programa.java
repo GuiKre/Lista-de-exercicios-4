9178d5391b623c0aac27a7d69ac24e31d2bdddcb
GuiKre <guilhermekre@gmail.com>
2024-04-25 08:51:27 -0300

    Cenario 02

--- Cenario02/src/br/edu/up/Programa.java
+++ Cenario02/src/br/edu/up/Programa.java
@@ -7,13 +7,25 @@ public class Programa {
         Ponto ponto1 = new Ponto();
         Ponto ponto2 = new Ponto(2.0, 5.0);
 
-        //Double distanciaPonto1Ponto2 = ponto1.calcularDistancia(ponto2);
+        System.out.println("---Ponto 01---");
+        System.out.println("Coordenada x: " + ponto1.getX());
+        System.out.println("Coordenada y: " + ponto1.getY());
+        System.out.println("\n");
+        System.out.println("---Ponto 02---");
+        System.out.println("Coordenada x: " + ponto2.getX());
+        System.out.println("Coordenada y: " + ponto2.getY());
+        System.out.println("\n");
+
         System.out.println("Distância entre o ponto 1 e ponto 2: " + ponto1.calcularDistancia(ponto2));
 
-        //Double distanciaPonto2Cord72 = ponto2.calcularDistanciaP(7.0, 2.0);
         System.out.println("Distância entre o ponto 2 e as coordenadas (7,2): " + ponto2.calcularDistanciaP(7.0, 2.0));
 
         ponto1.setX(10.0);
         ponto1.setY(3.0);
+
+        System.out.println("\n");
+        System.out.println("---Ponto 01 após alterações---");
+        System.out.println("Coordenada x: " + ponto1.getX());
+        System.out.println("Coordenada y: " + ponto1.getY());
     }
 }
