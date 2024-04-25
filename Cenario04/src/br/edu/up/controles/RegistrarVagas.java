d141083d065e98d58564daf8bdc8c39dd20bda0e
GuiKre <guilhermekre@gmail.com>
2024-04-25 09:45:29 -0300

    Cenario 04

---
+++ Cenario04/src/br/edu/up/controles/RegistrarVagas.java
@@ -0,0 +1,9 @@
+package br.edu.up.controles;
+import br.edu.up.modelos.Carro;
+
+public class RegistrarVagas {
+
+    Carro[] vagas = new Carro[10];
+
+    
+}
---
+++ Cenario04/src/br/edu/up/telas/Menu.java
@@ -0,0 +1,9 @@
+package br.edu.up.telas;
+import java.util.Scanner;
+
+public class Menu {
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("");
+
+}
